6c61381efe17b762567fc8aebeb134a9be5cf695
mayabytatech <maya.k.k@lxisoft.com>
2019-07-31 12:13:55 +0530

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/diviso/graeshoppe/report/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.diviso.graeshoppe.report.config.audit;
---
+++ src/main/java/com/diviso/graeshoppe/report/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.diviso.graeshoppe.report.repository;
+
+import com.diviso.graeshoppe.report.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
