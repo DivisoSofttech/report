efae485e18a83593093243c00286099a2e3b1112
mayabytatech <maya.k.k@lxisoft.com>
2019-08-01 10:09:28 +0530

    report method added

---
+++ src/main/java/com/diviso/graeshoppe/report/web/rest/ReportResource.java
@@ -0,0 +1,64 @@
+ /*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diviso.graeshoppe.report.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diviso.graeshoppe.report.service.dto.OrderMaster;
+
+/**
+ * TODO Provide a detailed description here 
+ * @author MayaSanjeev
+ * mayabytatech, maya.k.k@lxisoft.com
+ */
+
+@RestController
+@RequestMapping("/api/report")
+public class ReportResource {
+
+	@GetMapping("/order-report/{orderNumber}")
+	public OrderMaster getReportByOrderNumber(@PathVariable String orderNumber){
+		
+		OrderMaster orderMaster = new OrderMaster();
+		orderMaster.setMethodOfOrder("delivery");
+		orderMaster.setDate("02-Feb");
+		orderMaster.setTime("18:05");
+		orderMaster.setNotes("salt and vinegar");
+		orderMaster.setDeliveryCharge(2.00);
+		orderMaster.setServiceCharge(.50);
+		orderMaster.setOrderStatus("ORDER NOT PAID");
+		orderMaster.setCustomerId("22255545455");
+		orderMaster.setAddressType("home");
+		orderMaster.setCity("palakkad");
+		orderMaster.setHouseNoOrBuildingName("458");
+		orderMaster.setLandmark("new juma musjid");
+		orderMaster.setCity("palakkad");
+		orderMaster.setName("franklin");
+		orderMaster.setPhone(5446464646L);
+		orderMaster.setAlternatePhone(56566786545L);
+		orderMaster.setRoadNameAreaOrStreet("mujid road");
+		orderMaster.setPincode(4558885L);
+		orderMaster.setOrderFromCustomer(14);
+		orderMaster.setCustomersOrder(83);
+		orderMaster.setOrderPlaceAt("03-Feb 14:55");
+		orderMaster.setOrderAcceptedAt("03-Feb 15:05");
+		
+		return orderMaster;
+	}
+}
