c96ddda658871174046bb15c6dd816c30b495532
neerajamurali094 <neeraja.m@lxisoft.com>
2019-08-08 16:00:43 +0530

    jrxml file added

---
+++ order_line.sql
@@ -0,0 +1,5 @@
+insert into order_line (item, quantity, total, order_master_id) values ('massa donec dapibus duis at', 1, 23, 1);
+insert into order_line (item, quantity, total, order_master_id) values ('at turpis a pede posuere', 3, 74, 2);
+insert into order_line (item, quantity, total, order_master_id) values ('auctor gravida sem praesent id', 3, 10, 3);
+insert into order_line (item, quantity, total, order_master_id) values ('sed tristique in tempus sit amet', 3, 23, 4);
+insert into order_line (item, quantity, total, order_master_id) values ('eget congue eget semper rutrum nulla', 5, 9, 5);
---
+++ order_master.sql
@@ -0,0 +1,5 @@
+insert into order_master (store_name, store_phone, method_of_order) values ('luctus et ultrices posuere cubilia', '6336434655', 'potenti cras in purus eu');
+insert into order_master (store_name, store_phone, method_of_order) values ('nisi at nibh in hac habitasse', '7478701344', 'ante vivamus tortor duis mattis egestas');
+insert into order_master (store_name, store_phone, method_of_order) values ('tincidunt in leo maecenas pulvinar', '9525248174', 'mauris vulputate elementum nullam varius');
+insert into order_master (store_name, store_phone, method_of_order) values ('nunc vestibulum ante ipsum primis', '5843424528', 'luctus cum sociis natoque penatibus et');
+insert into order_master (store_name, store_phone, method_of_order) values ('ac consequat metus sapien ut nunc', '3653056321', 'tristique fusce congue diam id ornare');
