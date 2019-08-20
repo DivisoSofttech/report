d71f95fa38347bd178a02dcf6433e6b152d4184c
neerajamurali094 <neeraja.m@lxisoft.com>
2019-08-20 15:31:54 +0530

    report created

--- order_line.sql
+++ order_line.sql
@@ -1,11 +1,6 @@
-insert into order_line (item, quantity, total, order_master_id) values ('massa donec dapibus duis at', 1, 23, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('at turpis a pede posuere', 3, 74, 2);
-insert into order_line (item, quantity, total, order_master_id) values ('auctor gravida sem praesent id', 3, 10, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('sed tristique in tempus sit amet', 3, 23, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('eget congue eget semper rutrum nulla', 5, 9, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('easdfghjk', 15, 19, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('sdfghj', 25, 29, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('asdfghjkl', 35, 39, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('asdfghjkl', 45, 49, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('qwertyuiop', 55, 59, 1);
-insert into order_line (item, quantity, total, order_master_id) values ('zxcvbnm', 65, 69, 1);
+insert into order_line (item, quantity, total, order_master_id) values ('burger', 1, 6.83, 1);
+insert into order_line (item, quantity, total, order_master_id) values ('Chicken FriedRice', 1, 9.56, 1);
+insert into order_line (item, quantity, total, order_master_id) values ('margeritta pizza', 1, 23.00, 1);
+insert into order_line (item, quantity, total, order_master_id) values ('Chicken Soup', 2, 4.36, 2);
+insert into order_line (item, quantity, total, order_master_id) values ('Prawn FriedRice', 2, 36.12, 2);
+insert into order_line (item, quantity, total, order_master_id) values ('Ice cream', 3, 12.36, 2);
