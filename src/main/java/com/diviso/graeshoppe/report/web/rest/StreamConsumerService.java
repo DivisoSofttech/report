b374b5e107cfdf20a5c0122e22d696a067e0a2d2
abilash-diviso <abilash.s@lxisoft.com>
2019-09-05 16:22:06 +0530

    streamconsumer

--- src/main/java/com/diviso/graeshoppe/report/web/rest/StreamConsumerService.java
+++ src/main/java/com/diviso/graeshoppe/report/web/rest/StreamConsumerService.java
@@ -16,35 +16,36 @@ import com.diviso.graeshoppe.report.service.dto.OrderMasterDTO;
 @EnableBinding(MessageBinderConfiguration.class)
 public class StreamConsumerService {
 
-	private final Logger LOG = LoggerFactory.getLogger(StreamConsumerService.class);
-
+	private final Logger LOG=LoggerFactory.getLogger(StreamConsumerService.class);
+	
 	@Autowired
 	private OrderMasterService orderMasterService;
 
 	@StreamListener(MessageBinderConfiguration.PAYMENT)
 	public void listenToPayment(KStream<String, Payment> message) {
-		message.foreach((key, value) -> {
-			System.out.println("payment Value consumed is " + value);
-//			OrderMasterDTO orderMasterDTO = orderMasterService.findByOrderNumber(value.getTargetId()).get();
-//			if (orderMasterDTO != null) {
-//				if (!value.getPaymentType().equals("cod")) {
-//					LOG.info("Order paid");
-//					orderMasterDTO.setOrderStatus("ORDER PAID");
-//				} else {
-//					LOG.info("Order Not paid");
-//					orderMasterDTO.setOrderStatus("ORDER NOT PAID");
-//				}
-//				orderMasterService.save(orderMasterDTO);
-//			}
-	});
-	}
-
-	@StreamListener(MessageBinderConfiguration.ORDER)
-	public void listenToOrder(KStream<String, Order> message) {
-		message.foreach((key, value) -> {
-			System.out.println("order Value consumed is " + value);
-			orderMasterService.convertAndSaveOrderMaster(value);
+		message.foreach((key,value)->{
+				System.out.println("payment Value consumed is "+value);
+				OrderMasterDTO orderMasterDTO=orderMasterService.findByOrderNumber(value.getTargetId()).get();
+				if(orderMasterDTO!=null) {
+				if(!value.getPaymentType().equals("cod")) {
+					LOG.info("Order paid");
+					orderMasterDTO.setOrderStatus("ORDER PAID");
+				}else {
+					LOG.info("Order Not paid");
+					orderMasterDTO.setOrderStatus("ORDER NOT PAID");
+				}
+				orderMasterService.save(orderMasterDTO);
+				}
 		});
 	}
-
+	
+	@StreamListener(MessageBinderConfiguration.ORDER)
+	public void listenToOrder(KStream<String, Order> message) {
+		message.foreach((key,value)->{
+				System.out.println("order Value consumed is "+value);
+				orderMasterService.convertAndSaveOrderMaster(value);
+		});
+	}
+	
+	
 }
