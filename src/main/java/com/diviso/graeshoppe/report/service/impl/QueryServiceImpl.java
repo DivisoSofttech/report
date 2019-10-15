906deab1b40c0befcd4ba96bef2b8078557c9b39
sruthykc <sruthy.k.c@gmail.com>
2019-10-15 16:01:28 +0530

    test

--- src/main/java/com/diviso/graeshoppe/report/service/impl/QueryServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/report/service/impl/QueryServiceImpl.java
@@ -5,7 +5,9 @@ import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -189,18 +191,20 @@ public class QueryServiceImpl implements QueryService {
 	public OrderAggregator getOrderAggregator(String orderNumber) {
 
 		OrderAggregator orderAggregator= new OrderAggregator();
-		
-		orderAggregator.setOrderMaster(findOrderMasterByOrderNumber(orderNumber));
-		
-		orderAggregator.setOrderLine(findOrderLineByOrderMaster(orderAggregator.getOrderMaster().getId()));
+		OrderMaster master=findOrderMasterByOrderNumber(orderNumber);
+		orderAggregator.setOrderMaster(master);
+		Set<OrderLine> orderLines=new HashSet<OrderLine>(findOrderLineByOrderMaster(master.getId()));
+		orderAggregator.getOrderMaster().setOrderLines(orderLines);
 		
 		System.out.println("+++++++++++++++++++++++++++++++++++++++"+orderAggregator.getOrderLine());;
 		
 		
-		for(OrderLine orderLine : orderAggregator.getOrderLine()) {
+		for(OrderLine orderLine : orderAggregator.getOrderMaster().getOrderLines()) {
 			
-			orderAggregator.setComboItem(findComboItemByOrderLine(orderLine.getId()));
-			orderAggregator.setAuxitem(findAuxItemByOrderLine(orderLine.getId()));
+			List<ComboItem> combos=findComboItemByOrderLine(orderLine.getId());
+			List<AuxItem> auxes=findAuxItemByOrderLine(orderLine.getId());
+			orderLine.setComboItems(new HashSet<>(combos));
+			orderLine.setAuxItems(new HashSet<>(auxes));
 			
 		}
 		return orderAggregator;
