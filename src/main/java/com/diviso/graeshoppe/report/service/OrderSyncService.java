b8cb3c51909fed61c145686ccfcb8a96aafef7f9
abilash-diviso <abilash.s@lxisoft.com>
2020-01-24 11:40:59 +0530

    consuming order state from kafka and updating the report db

--- src/main/java/com/diviso/graeshoppe/report/service/OrderSyncService.java
+++ src/main/java/com/diviso/graeshoppe/report/service/OrderSyncService.java
@@ -2,7 +2,7 @@ package com.diviso.graeshoppe.report.service;
 
 import com.diviso.graeshoppe.report.config.KafkaProperties;
 import com.diviso.graeshoppe.report.service.dto.OrderMasterDTO;
-import com.diviso.graeshoppe.order.avro.ApprovalInfo;
+import com.diviso.graeshoppe.order.avro.*;
 import com.diviso.graeshoppe.avro.*;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.diviso.graeshoppe.order.avro.Order;
+
+import java.text.NumberFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
@@ -25,6 +27,8 @@ public class OrderSyncService {
 
 	private final Logger log = LoggerFactory.getLogger(OrderSyncService.class);
 
+	@Value("${topic.orderstate.destination}")
+	private String orderStateTopic;
 	@Value("${topic.order.destination}")
 	private String orderTopic;
 	@Value("${topic.approvalInfo.destination}")
@@ -41,6 +45,41 @@ public class OrderSyncService {
 		this.kafkaProperties = kafkaProperties;
 	}
 
+	public void subscribeToOrderState() {
+		log.debug("REST request to consume records from Kafka topics {}", orderStateTopic);
+		Map<String, Object> consumerProps = kafkaProperties.getConsumerProps();
+		sseExecutorService.execute(() -> {
+			KafkaConsumer<String, OrderState> consumer = new KafkaConsumer<>(consumerProps);
+			consumer.subscribe(Collections.singletonList(orderStateTopic));
+			boolean exitLoop = false;
+			while (!exitLoop) {
+				try {
+					ConsumerRecords<String, OrderState> records = consumer.poll(Duration.ofSeconds(5));
+					records.forEach(record -> {
+						log.info("orderStateTopic  is consuming " + record);
+						Optional<OrderMasterDTO> orderMaster = orderMasterService
+								.findByOrderNumber(record.value().getTargetId());
+						if (orderMaster.isPresent()) {
+							orderMaster.get().setOrderStatus(record.value().getState());
+							orderMasterService.save(orderMaster.get());
+						} else {
+							log.info("The order doesn't found with ref " + record.value().getTargetId());
+
+						}
+
+					});
+
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					log.trace("Complete with error {}", ex.getMessage(), ex);
+					exitLoop = true;
+				}
+			}
+			log.info("Consumer is going to close");
+			consumer.close();
+		});
+	}
+
 	public void subscribeToCancellationRequest() {
 		log.debug("REST request to consume records from Kafka topics {}", cancellationTopic);
 		Map<String, Object> consumerProps = kafkaProperties.getConsumerProps();
@@ -51,18 +90,23 @@ public class OrderSyncService {
 			while (!exitLoop) {
 				try {
 					ConsumerRecords<String, CancellationRequest> records = consumer.poll(Duration.ofSeconds(5));
+					NumberFormat formatter = NumberFormat.getInstance();
+					formatter.setMinimumFractionDigits(1);
+					formatter.setMinimumFractionDigits(2);
+					formatter.setMaximumFractionDigits(2);
 					records.forEach(record -> {
 						log.info("CancellationRequest  is consuming " + record);
 						Optional<OrderMasterDTO> orderMaster = orderMasterService
 								.findByOrderNumber(record.value().getOrderId());
 						if (orderMaster.isPresent()) {
-							orderMaster.get().setOrderStatus("cancellation-requested");
 							orderMaster.get().setRefundedAmount(record.value().getAmount());
-							orderMaster.get().setTotalDue(orderMaster.get().getTotalDue() - record.value().getAmount());
+							orderMaster.get().setTotalDue(Double.parseDouble(
+									formatter.format(orderMaster.get().getTotalDue() - record.value().getAmount())));
 							orderMaster.get().setCancellationRef(record.value().getId());
 							orderMasterService.save(orderMaster.get());
 						} else {
 							log.info("The order doesn't found with ref " + record.value().getOrderId());
+
 						}
 
 					});
@@ -142,6 +186,7 @@ public class OrderSyncService {
 		subscribeToApprovalInfo();
 		subscribeToOrder();
 		subscribeToCancellationRequest();
+		subscribeToOrderState();
 	}
 
 }
